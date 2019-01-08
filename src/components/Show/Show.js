1e83605be2ec6f01cf05fe0cbf1fe8773f7a3d16
DarkPnX <toblerone0073@gmail.com>
2019-01-08 23:28:28 +0300

    hw-CDU completed

---
+++ src/components/Show/Show.js
@@ -0,0 +1,55 @@
+import React, {Component } from 'react';
+import './Show.css';
+import {getShowInfo} from '../../api.js'
+
+class Show extends Component{
+    state = {
+        showId:'',
+        data:''
+    }
+
+    /*static getDerivedStateFromProps(nextProps,prevState){
+        if(prevState.showId === nextProps.showId){
+            return null;
+        }else{
+            return {
+                showId:nextProps.showId
+            }
+        }
+    }*/
+
+    componentDidUpdate = (prevState) => {
+        if(prevState.showId !== this.props.showId){
+            getShowInfo(this.props.showId)
+                .then(data =>{
+                    this.setState({showId:this.props.showId,data});
+                });
+        }
+    }
+    render(){
+        const {data} = this.state;
+        if(!data){
+            return (
+                <p className="t-show-info show-information">Шоу не выбрано</p>
+            );
+        }else{
+            return (
+                <div className="show">
+                    <img 
+                        src={data.image.medium}
+                        alt={data.name}
+                    />
+                    <h2 className="show-label t-show-name">{data.name}</h2>
+                    <p className="show-text t-show-genre">
+                        <b>Жанр: </b>
+                        {data.genres.join(', ')}
+                    </p>
+                    <p className="show-text t-show-summary" dangerouslySetInnerHTML={{ __html: data.summary }}></p>{/*Documentation(API REFERENCE/DOM Elements)*/}
+                </div>
+            );
+        }
+        
+    }
+}
+
+export default Show;
