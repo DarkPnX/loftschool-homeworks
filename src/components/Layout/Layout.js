cca0e1764252bef9d57fade0565dbeb99f43d650
DarkPnX <toblerone0073@gmail.com>
2019-01-13 17:55:23 +0300

    everything works, but does not pass the tests

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,35 @@
 import React, { Fragment, PureComponent } from 'react';
 import SectionTitle from '../SectionTitle';
 import './Layout.css';
+import { AuthProvider } from '../../contexts/Auth';
 
 class Layout extends PureComponent {
   render() {
-    return 'empty';
+    const {header,footer,children}=this.props;
+    return(
+      <Fragment>
+      {this.renderHeader(header)}
+      <main className="main main--with-header main--with-footer">
+        <SectionTitle className="main__title" children="main"></SectionTitle>
+        {children}
+      </main>
+      {this.renderFooter(footer)}
+      </Fragment>
+    );
   }
 
   renderHeader(HeaderChild) {
-    return 'empty';
+    return (<header className="header">
+      <SectionTitle className="header__title" children="header"></SectionTitle>
+      <HeaderChild/>
+    </header>);
   }
 
   renderFooter(FooterChild) {
-    return 'empty';
+    return (<footer className="footer">
+    <SectionTitle className="header__title" children="footer"></SectionTitle>
+    <FooterChild/>
+  </footer>);
   }
 }
 
--- src/contexts/Auth/Auth.js
+++ src/contexts/Auth/Auth.js
@@ -3,9 +3,49 @@ import React, { PureComponent } from 'react';
 const { Provider, Consumer: AuthConsumer } = React.createContext('');
 
 class AuthProvider extends PureComponent {
+  state = {
+    email: '',
+    authorizeError: '',
+    isAuthorized: false
+  }
+
+  authorize = (email,password) =>{
+    if(email==="stu@dent.com" && password==="123"){
+        this.setState({
+          email,
+          authorizeError:'',
+          isAuthorized:true
+        });
+    }else{
+        this.setState({
+          email:'',
+          authorizeError:'Email или пароль введён не верно',
+          isAuthorized:false
+        });
+    }
+  }
+
+  logout = () =>{
+    this.setState({
+      email:'',
+      authorizeError:'',
+      isAuthorized:false
+    });
+  }
+
+  getProviderValue = () =>{
+    return {
+      email: this.state.email,
+      authorizeError: this.state.authorizeError,
+      isAuthorized: this.state.isAuthorized,
+      authorize:this.authorize,
+      logout:this.logout
+    }
+  }
+
   render() {
     const { children } = this.props;
-    return <Provider>{children}</Provider>;
+    return <Provider value = {this.getProviderValue()}>{children}</Provider>;
   }
 }
 
